b854455666ece234449c39ec95ddf0ac5fcd5de7
SpiritedAwayLab <splxsgmobile@gmail.com>
2018-10-05 00:02:18 +0100

    X17

--- multihashing.cc
+++ multihashing.cc
@@ -7,6 +7,7 @@
 extern "C" {
    
     #include "argon2d-dyn.h"
+    #include "x17.h"
     #include "bcrypt.h"
     #include "blake.h"
     #include "c11.h"
@@ -53,6 +54,30 @@ extern "C" {
 
 using namespace node;
 using namespace v8;
+
+
+//X17
+NAN_METHOD(x17) {
+
+    if (info.Length() < 2)
+        return THROW_ERROR_EXCEPTION("You must provide two arguments.");
+
+    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
+
+    if(!Buffer::HasInstance(target))
+        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char *output = (char*) malloc(sizeof(char) * 32);
+
+    uint32_t input_len = Buffer::Length(target);
+
+    x17_hash(input, output, input_len);
+
+    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
+} 
+
+
 
 //argon2d-dyn
 NAN_METHOD(argon2d500) {
@@ -664,6 +689,7 @@ NAN_METHOD(yescrypt) {
 
 
 NAN_MODULE_INIT(init) {
+    Nan::Set(target, Nan::New("x17").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(x17)).ToLocalChecked());
     Nan::Set(target, Nan::New("argon2d500").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(argon2d500)).ToLocalChecked());
     Nan::Set(target, Nan::New("lyra2z").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(lyra2z)).ToLocalChecked());
     Nan::Set(target, Nan::New("lyra2rev2").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(lyra2rev2)).ToLocalChecked());
